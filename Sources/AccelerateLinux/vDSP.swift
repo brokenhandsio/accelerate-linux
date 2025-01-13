398a3d8a190850d7a924c77f3a6bdf96f2ba2a25
Paul Toffoloni <paultoffoloni@gmail.com>
2025-01-13 17:07:30 +0000

    Add `vDSP_mtransD` transposition function

--- Sources/AccelerateLinux/vDSP.swift
+++ Sources/AccelerateLinux/vDSP.swift
@@ -37,9 +37,36 @@ public func vDSP_minvD(
 ) {
     __C.pointee = Double.infinity
     guard __N > 0 else { return }
-    __C.pointee = __A.pointee
     for i in 1..<__N {
         __C.pointee = min(__C.pointee, __A[Int(i) * __I])
+    }
+}
+
+/// Transposes a double-precision matrix.
+/// - Parameters:
+///   - __A: The input matrix.
+///   - __IA: The stride between elements in the input matrix.
+///   - __C: The output matrix.
+///   - __IC: The stride between elements in the output matrix.
+///   - __M: The number of rows in the output matrix and the number of columns in the input matrix.
+///   - __N: The number of columns in the output matrix and the number of rows in the input matrix.
+/// - Warning: This function doesn’t support in-place operation.
+public func vDSP_mtransD(
+    _ __A: UnsafePointer<Double>,
+    _ __IA: vDSP_Stride,
+    _ __C: UnsafeMutablePointer<Double>,
+    _ __IC: vDSP_Stride,
+    _ __M: vDSP_Length,
+    _ __N: vDSP_Length
+) {
+    var __A = __A
+    for i in 0..<__N {
+        var __C = __C.advanced(by: Int(i) * __IC)
+        for _ in 0..<__M {
+            __C.pointee = __A.pointee
+            __A = __A.advanced(by: __IA)
+            __C = __C.advanced(by: Int(__N) * __IC)
+        }
     }
 }
 #endif
--- Tests/AccelerateLinuxTests/vDSPTests.swift
+++ Tests/AccelerateLinuxTests/vDSPTests.swift
@@ -24,26 +24,48 @@ struct vDSPTests {
         vDSP_maxvD(a, stride, &c, n)
         #expect(c == -Double.infinity)
     }
-    
+
     @Test("minvD Simple")
     func vDSP_minvDSimpleTest() async throws {
         let stride = vDSP_Stride(1)
         let a: [Double] = [-1.5, 2.25, 3.6, 0.2, -0.1, -4.3]
         let n = vDSP_Length(a.count)
         var c = Double()
-        
+
         vDSP_minvD(a, stride, &c, n)
         #expect(c == -4.3)
     }
-    
+
     @Test("minvD Empty Array")
     func vDSP_minvDEmptyArrayTest() async throws {
         let stride = vDSP_Stride(1)
         let a: [Double] = []
         let n = vDSP_Length(a.count)
         var c = Double()
-        
+
         vDSP_minvD(a, stride, &c, n)
         #expect(c == Double.infinity)
+    }
+
+    @Test("mtransD")
+    func vDSP_mtransDTest() async throws {
+        let source: [Double] = [
+            1, 2, 3, 4,
+            5, 6, 7, 8,
+        ]
+
+        let transposed = [Double](unsafeUninitializedCapacity: source.count) { buffer, unsafeUninitializedCapacity in
+            vDSP_mtransD(source, 1, buffer.baseAddress!, 1, 4, 2)
+            unsafeUninitializedCapacity = source.count
+        }
+
+        #expect(
+            transposed == [
+                1, 5,
+                2, 6,
+                3, 7,
+                4, 8,
+            ]
+        )
     }
 }
