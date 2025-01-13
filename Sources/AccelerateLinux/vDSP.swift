30004e33b6b6545e00fad94bbf57463f9e94db82
Paul Toffoloni <paultoffoloni@gmail.com>
2025-01-13 16:22:44 +0000

    Add vDSP_minvD tests

--- Sources/AccelerateLinux/vDSP.swift
+++ Sources/AccelerateLinux/vDSP.swift
@@ -25,16 +25,17 @@ public func vDSP_maxvD(
 
 /// Calculates the double-precision minimum value of a vector.
 /// - Parameters:
-/// - __A: The vector to be examined.
-/// - __I: The stride of the vector.
-/// - __C: The minimum value.
-/// - __N: The number of elements in the vector.
+///   - __A: The vector to be examined.
+///   - __I: The stride of the vector.
+///   - __C: The minimum value.
+///   - __N: The number of elements in the vector.
 public func vDSP_minvD(
     _ __A: UnsafePointer<Double>,
     _ __I: vDSP_Stride,
     _ __C: UnsafeMutablePointer<Double>,
     _ __N: vDSP_Length
 ) {
+    __C.pointee = Double.infinity
     guard __N > 0 else { return }
     __C.pointee = __A.pointee
     for i in 1..<__N {
--- Tests/AccelerateLinuxTests/vDSPTests.swift
+++ Tests/AccelerateLinuxTests/vDSPTests.swift
@@ -15,7 +15,7 @@ struct vDSPTests {
     }
 
     @Test("maxvD Empty Array")
-    func vDSP_minvDEmptyArrayTest() async throws {
+    func vDSP_maxvDEmptyArrayTest() async throws {
         let stride = vDSP_Stride(1)
         let a: [Double] = []
         let n = vDSP_Length(a.count)
@@ -24,5 +24,26 @@ struct vDSPTests {
         vDSP_maxvD(a, stride, &c, n)
         #expect(c == -Double.infinity)
     }
-
+    
+    @Test("minvD Simple")
+    func vDSP_minvDSimpleTest() async throws {
+        let stride = vDSP_Stride(1)
+        let a: [Double] = [-1.5, 2.25, 3.6, 0.2, -0.1, -4.3]
+        let n = vDSP_Length(a.count)
+        var c = Double()
+        
+        vDSP_minvD(a, stride, &c, n)
+        #expect(c == -4.3)
+    }
+    
+    @Test("minvD Empty Array")
+    func vDSP_minvDEmptyArrayTest() async throws {
+        let stride = vDSP_Stride(1)
+        let a: [Double] = []
+        let n = vDSP_Length(a.count)
+        var c = Double()
+        
+        vDSP_minvD(a, stride, &c, n)
+        #expect(c == Double.infinity)
+    }
 }
